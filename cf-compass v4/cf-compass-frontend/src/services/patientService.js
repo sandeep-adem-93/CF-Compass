fdcde91d021e6c56582125704df34312e9944d9a
Sandeep Adem <sadem3@gatech.edu>
2025-04-13 23:20:50 -0700

    added render backend connection env variable

--- cf-compass v4/cf-compass-frontend/src/services/patientService.js
+++ cf-compass v4/cf-compass-frontend/src/services/patientService.js
@@ -1,7 +1,8 @@
 // src/services/patientService.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Get all patients
 export const getPatients = async () => {
